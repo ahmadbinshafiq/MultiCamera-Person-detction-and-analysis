e34ca4ccc83dcca7c8fc92f15270272f1bb7d6bc
HamzaQazi10 <hamzaarshadqazi@gmail.com>
2022-04-25 14:06:55 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import New from './dummycomponent/New';
+import Card from './dummycomponent/Card'
+function App() {
+  return (
+    <div
+        class="bg_image"
+        style={{
+          backgroundImage: 'url("https://th.bing.com/th/id/OIP.nfpQv_isCYml-SajqDk-LQHaEo?pid=ImgDet&rs=1")',
+          backgroundSize: "cover",
+          height: "100vh",
+          color: "#f5f5f5"
+        }}
+      >
+      {/* <img src="https://th.bing.com/th/id/OIP.nfpQv_isCYml-SajqDk-LQHaEo?pid=ImgDet&rs=1" alt="..."/> */}
+      <New></New>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
